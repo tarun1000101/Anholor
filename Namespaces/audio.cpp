a8fee2a5630b59fd6795649e0c6ff7ffbe779d10
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-03-06 16:16:47 +0530

    Changed files and renamed files

---
+++ Namespaces/audio.cpp
@@ -0,0 +1,8 @@
+namespace audio{
+    struct audio_memory_block{
+        long long int length_of_block;
+        uint64_t MIDI_signal_block[length_of_block];
+        uint64_t MIDI_amplification_block[length_of_block];
+        uint64_t 
+    };
+}
