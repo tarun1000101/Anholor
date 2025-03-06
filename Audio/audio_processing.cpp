20c9c8b984d6a3701c2edee354c57df15f606c1e
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-03-06 15:39:15 +0530

    Added a class for hashing

---
+++ Audio/audio_processing.cpp
@@ -0,0 +1,20 @@
+struct memory_block{
+    long long int length_of_block
+    uint64_t memory_block[length_of_block];
+};
+
+memory_block recieve_and_return_audio_signal(uint64_t port, long long int length_of_audio_block)//Audio ports are attached to certain memory addresses in RAM.
+{
+    /** Recieve and return audio signal
+     * @param port - port address in RAM.
+     * @param length_of_audio_block - number of memory addresses to be allocated for processing the audio.
+     */
+
+    memory_block audio_block = new memory_block();
+    memory_block.length_of_block = length_of_audio_block;
+    
+    for (int i = 0; i < length_of_audio_block; i++)
+        audio_block.memory_block[i] = &port;
+
+    return audio_block;
+}
