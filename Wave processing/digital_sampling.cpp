a0bc8395327b2667d48267622934ef465492188a
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-03-06 15:15:33 +0530

    Added a function for ADC

--- Wave processing/digital_sampling.cpp
+++ Wave processing/digital_sampling.cpp
@@ -1,4 +1,5 @@
 class sampling{
+    
     double* fourier_transform(double *wave_levels)
     {
         /**
@@ -13,5 +14,11 @@ class sampling{
         }
 
         return samples;
+    }
+
+    double ADC (double voltage_level, double bit_depth)
+    {
+        double digital_value = voltage_level/bit_depth;
+        return digital_value;
     }
 };
