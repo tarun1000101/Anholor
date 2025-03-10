39e0f4e78a90ad395b4f73fac2d46d0aea0df6ea
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-03-10 21:34:02 +0530

    Changed datatype of process id

--- Firmware/Namespaces/process.cpp
+++ Firmware/Namespaces/process.cpp
@@ -1,6 +1,6 @@
 namespace process{
     typedef struct process{
-        long long int process_id;// A pointer to the process id stored in RAM.
+        char *process_id;// A pointer to the process id stored in RAM.
         char *process_name;
         char *process_block;
         vector<char*> services_offered;
