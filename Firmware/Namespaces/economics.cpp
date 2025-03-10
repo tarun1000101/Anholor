990fe4891a19dc486201ba937f9d1e185e66be9a
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-03-10 20:47:31 +0530

    Added types for cash flows, rate of return and IRR

--- Firmware/Namespaces/economics.cpp
+++ Firmware/Namespaces/economics.cpp
@@ -1,6 +1,34 @@
 namespace economics{
+    
     typedef struct cash flow{
-         number_of_Cash_flows;
-
+        double2048_t number_of_cash_flows;
+        double2048_t cash_flows[number_of_cash_flows];
     };
+
+    typedef struct single_payment_future_amount{
+        double2048_t principal;
+        double2048_t interest_rate;
+        double2048_t payment_period;
+        double2048_t future_amount;
+    };
+
+    typedef struct uniform_annual_payments_future_amount{
+        double2048_t principal;
+        double2048_t number_of_cash_flows;
+        double2048_t cash_flows[number_of_cash_flows];
+        double2048_t interest_rate;
+        double2048_t payment_period;
+        double2048_t future_amount;
+    };
+
+    typedef struct annual_payments_future_amount{
+        double2048_t principal;
+        double2048_t number_of_cash_flows;
+        double2048_t cash_flows[number_of_cash_flows];
+        double2048_t interest_rate;
+        double2048_t payment_period;
+        double2048_t future_amount;
+    };
+
+    typedef struct 
 }
