5ae456c21c0bfa83920c77b0bdf36cc149d110ff
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-03-10 14:47:05 +0530

    Changed namespaces

--- Firmware/Namespaces/physics.cpp
+++ Firmware/Namespaces/physics.cpp
@@ -1,4 +1,58 @@
 namespace physics{
+
+    typedef struct object_data{
+        uint1024_t image [7680][4320];
+        uint1024_t x_coordinate;
+        uint1024_t y_coordinate;
+        uint1024_t z_coordinate;
+        uint1024_t electric_field;
+        uint1024_t magnetic_field;
+        uint1024_t radius_of_EM_field;
+        uint1024_t frequency_of_vibration;
+        uint1024_t radiation_of_object;
+        uint1024_t weight_of_object;
+        uint1024_t number_of_vertices;
+        uint1024_t graph_of_object [number_of_vertices];
+        uint1024_t mass_of_object;
+    };
+
+    typedef struct object_free_body_motion{
+        uint1024_t gyration;
+        uint1024_t gravitational_pull;
+        uint1024_t accleration;
+    };
+
+    typedef struct accelrometer{
+        uint1024_t macroscopic_change_in_x_coordinate;
+        uint1024_t macroscopic_change_in_y_coordinate;
+        uint1024_t macroscopic_change_in_z_coordinate;
+        uint1024_t microscopic_change_in_x_coordinate;
+        uint1024_t microscopic_change_in_y_coordinate;
+        uint1024_t microscopic_change_in_z_coordinate;
+    };
+
+    typedef struct gyroscope{
+        uint1024_t change_in_x_degree;
+        uint1024_t change_in_y_degree;
+        uint1024_t change_in_z_degree;
+    };
+
+    typedef struct energy_in_an_object{
+        uint1024_t height_of_an_object;
+        uint1024_t pressure_exerted_by_an_object;
+        uint1024_t gravitation;
+        uint1024_t speed_of_light;
+        uint1024_t energy_in_an_object = ((height_of_an_object * pressure_exerted_by_an_object * speed_of_light)/ gravitation);
+    };
+
+    typedef struct gravitational_field_of_an_object{
+        const double universal_gravitational_constant = 9.8;
+        uint1024_t mass_of_object1;
+        uint1024_t mass_of_object2;
+        uint1024_t distance_between_objects;
+        uint1024_t gravitiational_constant_of_an_object = (universal_gravitational_constant * mass_of_object1 * mass_of_object2)/distance_between_objects;
+    };
+
     typedef struct free_body_motion_of_an_object{
         uint1024_t gyration;
         uint1024_t gravitational_pull;
