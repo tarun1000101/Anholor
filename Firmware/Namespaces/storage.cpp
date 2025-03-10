7ce71f8f4d846e9afb3c922cf3a77c6b79c04721
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-03-10 21:17:32 +0530

    Added firmware for filesystems

---
+++ Firmware/Namespaces/storage.cpp
@@ -0,0 +1,13 @@
+namespace storage{
+    typedef struct storage_block{
+        char *data_stream;
+    }
+
+    typedef struct filesystem_database{
+        char *database_name;
+        uint2048_t number_of_clusters;
+        uint2048_t number_of_entries;
+        char cluster_names[number_of_clusters];
+        uint2048_t filesystem_matrix[number_of_clusters][number_of_entries];
+    };
+}
