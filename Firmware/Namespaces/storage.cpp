e12d34544a127e0f7dcd6e2c273aaa08fbcd44ed
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-03-10 21:19:58 +0530

    Finished firmware

--- Firmware/Namespaces/storage.cpp
+++ Firmware/Namespaces/storage.cpp
@@ -9,5 +9,7 @@ namespace storage{
         uint2048_t number_of_entries;
         char cluster_names[number_of_clusters];
         uint2048_t filesystem_matrix[number_of_clusters][number_of_entries];
+        vector<uint2048_t> filesystem_forest;
+        vector<uint2048_t> data_forest;
     };
 }
